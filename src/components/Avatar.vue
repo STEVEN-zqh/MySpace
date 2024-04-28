697f73f81238020c344a1bb121eb27fe877c5280
ZQH\Administrator <878994255@qq.com>
2024-04-28 14:55:22 +0800

    配置

---
+++ src/components/Avatar.vue
@@ -0,0 +1,25 @@
+<template>
+  <img class="avater-img" :src="url" :style="{ width:size+'px', height:size+'px', }" />
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      default: 100,
+    },
+  },
+};
+</script>
+
+<style>
+.avater-img{
+    border-radius: 50%;
+    object-fit: cover;
+}
+</style>
